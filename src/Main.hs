daf4dfc56dd4c676c46bee19eb5f8fd4793e6ffc
Rune K. Svendsen <runesvend@gmail.com>
2016-07-05 02:05:21 +0200

    Remember to always lift with your legs, and not your back, when you liftIO

--- src/Main.hs
+++ src/Main.hs
@@ -34,9 +34,9 @@ appInit = makeSnaplet "BlockchainAddressIndex" "Blockchain RESTful address index
     rpcConf <- liftIO . getRPCConf =<< getSnapletUserConfig
     liftIO $ putStrLn $ "Using Bitcoin Core endpoint: " ++
             rpcHost rpcConf ++ ":" ++ show (rpcPort rpcConf)
-    liftIO $ putStr $ "Performing test request... "
-    liftIO . putStrLn . ("Success: " ++) . show =<< liftIO (getUnredeemedOutputs
-            rpcConf (b58String "2N414xMNQaiaHCT5D7JamPz7hJEc9RG7469"))
+    liftIO $ putStr "Performing test request... " >>
+            getUnredeemedOutputs rpcConf testAddr >>=
+            putStrLn . ("Success! " ++) . show
     addRoutes [ ("unspentOutputs/:address", unspentOutputHandler rpcConf) ]
 
 
@@ -64,8 +64,7 @@ errorWithDescription code errStr = do
     modifyResponse $ setResponseStatus code (cs errStr)
     finishWith =<< getResponse
 
-
-
+testAddr = b58String "2N414xMNQaiaHCT5D7JamPz7hJEc9RG7469"
 
 
 
