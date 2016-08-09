e9760de077f083a68b9cf8c111ff421fed1fd921
Rune K. Svendsen <runesvend@gmail.com>
2016-08-09 20:22:42 +0200

    more Orphans & export them too

--- src/APISpec/Types.hs
+++ src/APISpec/Types.hs
@@ -1,7 +1,9 @@
 module APISpec.Types
 (
     AddressFundingInfo(..)
+  , module Orphans
 )
 where
 
 import Lib.FundingInfo.Types
+import Orphans
--- src/Orphans.hs
+++ src/Orphans.hs
@@ -5,6 +5,7 @@ module Orphans where
 import qualified Network.Haskoin.Crypto as HC
 import qualified Network.Haskoin.Transaction as HT
 import qualified Network.Haskoin.Util as HU
+import qualified Data.Binary as Bin
 
 import qualified Web.HttpApiData as Web
 import qualified Servant.API.ContentTypes as Content
@@ -23,7 +24,15 @@ instance Content.MimeUnrender Content.PlainText HT.Tx where
     mimeUnrender _ bs = decodeHex (cs bs) >>=
              fmapL ("failed to decode transaction: " ++) . HU.decodeToEither
 
+instance Content.MimeRender Content.PlainText HT.Tx where
+    mimeRender _ = cs . HU.encodeHex . cs . Bin.encode
+
 instance Content.MimeRender Content.PlainText HT.TxHash where
     mimeRender _ = cs . HT.txHashToHex
+
+instance Content.MimeUnrender Content.PlainText HT.TxHash where
+    mimeUnrender _ bs = maybe
+        (Left "failed to parse Bitcoin address") Right $
+            HT.hexToTxHash (cs bs)
 
 
